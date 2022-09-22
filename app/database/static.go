939fcd1b50006523842ba8a0771a7ae3436b103a
MeidoNoHitsuji <popenkodaniil@mail.ru>
2022-09-22 20:22:20 +0700

    feat: Добавлен фильтр ивентов в подписках

--- app/database/static.go
+++ app/database/static.go
@@ -141,6 +141,54 @@ func GetSubscribesByProjectIdAndKind(filter GetSubscribesFilter) []models.Subscr
 
 	builder = builder.Find(&subscribes)
 
+	for _, subscribe := range subscribes {
+		var events []models.SubscribeEvent
+		for _, event := range subscribe.Events {
+			if filter.Event != "" && filter.Event != event.Event {
+				continue
+			}
+
+			if filter.Status != "" {
+				obj, ok := event.Parameters["status"]
+				if ok && !helper.Contains(obj, filter.Status) {
+					continue
+				}
+			}
+
+			if filter.AuthorUsername != "" {
+				obj, ok := event.Parameters["author_username"]
+				if ok && !helper.Contains(obj, filter.AuthorUsername) {
+					continue
+				}
+			}
+
+			if filter.ToBranchName != "" {
+				obj, ok := event.Parameters["to_branch_name"]
+				if ok && !helper.Contains(obj, filter.ToBranchName) {
+					continue
+				}
+			}
+
+			if filter.FromBranchName != "" {
+				obj, ok := event.Parameters["from_branch_name"]
+				if ok && !helper.Contains(obj, filter.FromBranchName) {
+					continue
+				}
+			}
+
+			if filter.IsMerge != "" {
+				obj, ok := event.Parameters["is_merge"]
+				if ok && !helper.Contains(obj, filter.IsMerge) {
+					continue
+				}
+			}
+
+			events = append(events, event)
+		}
+
+		subscribe.Events = events
+	}
+
 	return subscribes
 }
 
