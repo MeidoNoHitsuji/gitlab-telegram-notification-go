8054054373542f5b058cc0536877e494b0fd5e97
Meido <popenkodaniil@mail.ru>
2022-09-28 00:38:23 +0700

    feat: Отредактирован запрос создания времени jira

--- app/jiraclient/static.go
+++ app/jiraclient/static.go
@@ -26,13 +26,6 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 		return
 	}
 
-	db := database.Instant()
-	var eventIntegration models.ToggleJiraIntegration
-
-	res := db.Where(models.ToggleJiraIntegration{
-		TimeEntityId: data.Payload.Id,
-	}).Find(&eventIntegration)
-
 	issue, _, err := client.Issue.Get(issueKey, &jira.GetQueryOptions{})
 
 	if err != nil {
@@ -49,7 +42,7 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 	me, _, err := client.User.GetSelf()
 
 	if err != nil {
-		fmt.Println("Ошибка самого себя")
+		fmt.Println("Ошибка получения самого себя")
 		fmt.Println(err)
 		return
 	}
@@ -58,6 +51,13 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 		fmt.Println("Попытка отредактировать задачу, которая не принадлежит тебе")
 		return
 	}
+
+	db := database.Instant()
+	var eventIntegration models.ToggleJiraIntegration
+
+	res := db.Where(models.ToggleJiraIntegration{
+		TimeEntityId: data.Payload.Id,
+	}).Find(&eventIntegration)
 
 	if res.RowsAffected == 0 {
 		eventIntegration.TimeEntityId = data.Payload.Id
@@ -70,7 +70,6 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 		}
 
 		worklogRecord, _, err := client.Issue.AddWorklogRecord(issue.ID, &jira.WorklogRecord{
-			Author:           me,
 			Started:          getTime(data.Payload.Start),
 			TimeSpentSeconds: data.Payload.Duration,
 		})
@@ -99,5 +98,57 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 }
 
 func DeleteJiraWorklog(telegramChannelId int64, data request.ToggleData) {
-
+	//issueKey := GetIssueKeyFromText(data.Payload.Description)
+	//
+	//if issueKey == "" {
+	//	fmt.Println("Ключ не найден!!")
+	//	return
+	//}
+	//
+	//client, err := New(telegramChannelId)
+	//
+	//if err != nil {
+	//	fmt.Println("Ошибка создания слиента")
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//db := database.Instant()
+	//var eventIntegration models.ToggleJiraIntegration
+	//
+	//res := db.Where(models.ToggleJiraIntegration{
+	//	TimeEntityId: data.Payload.Id,
+	//}).Find(&eventIntegration)
+	//
+	//if res.RowsAffected == 0 {
+	//	return
+	//}
+	//
+	//issue, _, err := client.Issue.Get(fmt.Sprintf("%d", eventIntegration.IssueId), &jira.GetQueryOptions{})
+	//
+	//if err != nil {
+	//	fmt.Println("Ошибка запроса задачи " + issueKey)
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//if strings.ToLower(issue.Fields.Status.Name) == "закрыто" {
+	//	fmt.Println("Нельзя редактировать закрытые задачи")
+	//	return
+	//}
+	//
+	//me, _, err := client.User.GetSelf()
+	//
+	//if err != nil {
+	//	fmt.Println("Ошибка получения самого себя")
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//if issue.Fields.Assignee.AccountID != me.AccountID {
+	//	fmt.Println("Попытка отредактировать задачу, которая не принадлежит тебе")
+	//	return
+	//}
+	//
+	//TODO: Придётся удалять через вызов request (https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-worklogs/#api-rest-api-3-issue-issueidorkey-worklog-id-delete)
 }
