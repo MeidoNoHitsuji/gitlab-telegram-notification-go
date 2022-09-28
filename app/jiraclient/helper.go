3dfe68e3665452a416c4a9531ea398df8ea0e885
MeidoNoHitsuji <popenkodaniil@mail.ru>
2022-09-28 15:46:02 +0700

    feat: Добавлено обновление и удаление времени jira

--- app/jiraclient/helper.go
+++ app/jiraclient/helper.go
@@ -1,6 +1,8 @@
 package jiraclient
 
 import (
+	"context"
+	"fmt"
 	"github.com/andygrunwald/go-jira"
 	"regexp"
 	"time"
@@ -21,4 +23,21 @@ func getTime(original time.Time) *jira.Time {
 	jiraTime := jira.Time(original)
 
 	return &jiraTime
+}
+
+func DeleteWorklogRecord(c *jira.Client, issueID string, worklogID string) (*jira.Response, error) {
+	apiEndpoint := fmt.Sprintf("rest/api/2/issue/%s/worklog/%s", issueID, worklogID)
+	req, err := c.NewRequestWithContext(context.Background(), "DELETE", apiEndpoint, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Do(req, nil)
+	if err != nil {
+		jerr := jira.NewJiraError(resp, err)
+		return resp, jerr
+	}
+
+	return resp, nil
 }
--- app/jiraclient/static.go
+++ app/jiraclient/static.go
@@ -58,6 +58,14 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 		return
 	}
 
+	IssueId, err := strconv.Atoi(issue.ID)
+
+	if err != nil {
+		fmt.Println("Ошибка парсинга issue.ID - " + issue.ID)
+		fmt.Println(err)
+		return
+	}
+
 	db := database.Instant()
 	var eventIntegration models.ToggleJiraIntegration
 
@@ -67,13 +75,7 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 
 	if res.RowsAffected == 0 {
 		eventIntegration.TimeEntityId = data.Payload.Id
-		eventIntegration.IssueId, err = strconv.Atoi(issue.ID)
-
-		if err != nil {
-			fmt.Println("Ошибка парсинга issue.ID - " + issue.ID)
-			fmt.Println(err)
-			return
-		}
+		eventIntegration.IssueId = IssueId
 
 		worklogRecord, _, err := client.Issue.AddWorklogRecord(issue.ID, &jira.WorklogRecord{
 			Comment:          "Created by TelegramBot",
@@ -96,66 +98,116 @@ func UpdateJiraWorklog(telegramChannelId int64, data request.ToggleData) {
 		}
 
 		db.Save(&eventIntegration)
-
-		fmt.Println("IT'S WORK!!")
 	} else {
+		if eventIntegration.IssueId != IssueId {
+			fmt.Println("Пересоздаём карточку") //Мне лень расписывать отдельно по запросам
+			DeleteJiraWorklog(telegramChannelId, data)
+			UpdateJiraWorklog(telegramChannelId, data)
+		} else {
+			IssueId := strconv.Itoa(eventIntegration.IssueId)
+			WorklogId := strconv.Itoa(eventIntegration.WorklogRecordId)
 
+			_, _, err := client.Issue.UpdateWorklogRecord(IssueId, WorklogId, &jira.WorklogRecord{
+				Started:          getTime(data.Payload.Start),
+				TimeSpentSeconds: tmath.MaxI(data.Payload.Duration, 60),
+			})
+
+			if err != nil {
+				fmt.Println("Ошибка обновлнения времени")
+				fmt.Println(err)
+				return
+			} else {
+				fmt.Println("Время обновлено!")
+				return
+			}
+		}
 	}
-
 }
 
 func DeleteJiraWorklog(telegramChannelId int64, data request.ToggleData) {
-	//issueKey := GetIssueKeyFromText(data.Payload.Description)
-	//
-	//if issueKey == "" {
-	//	fmt.Println("Ключ не найден!!")
-	//	return
-	//}
-	//
-	//client, err := New(telegramChannelId)
-	//
-	//if err != nil {
-	//	fmt.Println("Ошибка создания слиента")
-	//	fmt.Println(err)
-	//	return
-	//}
-	//
-	//db := database.Instant()
-	//var eventIntegration models.ToggleJiraIntegration
-	//
-	//res := db.Where(models.ToggleJiraIntegration{
-	//	TimeEntityId: data.Payload.Id,
-	//}).Find(&eventIntegration)
-	//
-	//if res.RowsAffected == 0 {
-	//	return
-	//}
-	//
-	//issue, _, err := client.Issue.Get(fmt.Sprintf("%d", eventIntegration.IssueId), &jira.GetQueryOptions{})
-	//
-	//if err != nil {
-	//	fmt.Println("Ошибка запроса задачи " + issueKey)
-	//	fmt.Println(err)
-	//	return
-	//}
-	//
-	//if strings.ToLower(issue.Fields.Status.Name) == "закрыто" {
-	//	fmt.Println("Нельзя редактировать закрытые задачи")
-	//	return
-	//}
-	//
-	//me, _, err := client.User.GetSelf()
-	//
-	//if err != nil {
-	//	fmt.Println("Ошибка получения самого себя")
-	//	fmt.Println(err)
-	//	return
-	//}
-	//
-	//if issue.Fields.Assignee.AccountID != me.AccountID {
-	//	fmt.Println("Попытка отредактировать задачу, которая не принадлежит тебе")
-	//	return
-	//}
-	//
-	//TODO: Придётся удалять через вызов request (https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-worklogs/#api-rest-api-3-issue-issueidorkey-worklog-id-delete)
+	issueKey := GetIssueKeyFromText(data.Payload.Description)
+
+	if issueKey == "" {
+		fmt.Println("Ключ не найден!!")
+		return
+	}
+
+	client, err := New(telegramChannelId)
+
+	if err != nil {
+		fmt.Println("Ошибка создания клиента")
+		fmt.Println(err)
+		return
+	}
+
+	db := database.Instant()
+	var eventIntegration models.ToggleJiraIntegration
+
+	res := db.Where(models.ToggleJiraIntegration{
+		TimeEntityId: data.Payload.Id,
+	}).Find(&eventIntegration)
+
+	if res.RowsAffected == 0 {
+		return
+	}
+
+	IssueId := strconv.Itoa(eventIntegration.IssueId)
+	WorklogId := strconv.Itoa(eventIntegration.WorklogRecordId)
+
+	worklogs, _, err := client.Issue.GetWorklogs(IssueId)
+
+	if err != nil {
+		fmt.Println("Ошибка получения ворклогов " + issueKey)
+		return
+	}
+
+	found := false
+
+	for _, worklog := range worklogs.Worklogs {
+		if worklog.ID == WorklogId {
+			found = true
+		}
+	}
+
+	if !found {
+		db.Delete(&eventIntegration)
+		fmt.Println("Ворклог не был найден в jira и был удалён!")
+		return
+	}
+
+	issue, _, err := client.Issue.Get(IssueId, &jira.GetQueryOptions{})
+
+	if err != nil {
+		fmt.Println("Ошибка запроса задачи " + issueKey)
+		fmt.Println(err)
+		return
+	}
+
+	if strings.ToLower(issue.Fields.Status.Name) == "закрыто" {
+		fmt.Println("Нельзя редактировать закрытые задачи")
+		return
+	}
+
+	me, _, err := client.User.GetSelf()
+
+	if err != nil {
+		fmt.Println("Ошибка получения самого себя")
+		fmt.Println(err)
+		return
+	}
+
+	if issue.Fields.Assignee.AccountID != me.AccountID {
+		fmt.Println("Попытка отредактировать задачу, которая не принадлежит тебе")
+		return
+	}
+
+	_, err = DeleteWorklogRecord(client, IssueId, WorklogId)
+	if err != nil {
+		fmt.Println("Ворклог не удалось удалить!")
+		fmt.Println(err)
+		return
+	}
+
+	db.Delete(&eventIntegration)
+	fmt.Println("Ворклог удалён!")
 }
