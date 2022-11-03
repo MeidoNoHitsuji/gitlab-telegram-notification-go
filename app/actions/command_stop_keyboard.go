8b956391564755d64124dde06606440f27bb8850
MeidoNoHitsuji <popenkodaniil@mail.ru>
2022-11-03 13:59:42 +0700

    feat: Добавил команду удаления клавиатуры

---
+++ app/actions/command_stop_keyboard.go
@@ -0,0 +1,28 @@
+package actions
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gitlab-telegram-notification-go/telegram"
+)
+
+const StopKeyboard ActionNameType = "sk"
+
+type StopKeyboardAction struct {
+	BaseAction
+}
+
+func NewStopKeyboardAction() *StopKeyboardAction {
+	return &StopKeyboardAction{
+		BaseAction: BaseAction{
+			ID:       Say,
+			InitBy:   []ActionInitByType{InitByCommand},
+			InitText: "stop_keyboard",
+		},
+	}
+}
+
+func (act *StopKeyboardAction) Active(update tgbotapi.Update) error {
+	message, _ := telegram.GetMessageFromUpdate(update)
+	telegram.SendRemoveKeyboard(message.Chat.ID, false)
+	return nil
+}
